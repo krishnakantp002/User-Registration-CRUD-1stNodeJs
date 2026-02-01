fab067050e0a7102710b02b9c8aeb5b8490bf699
Krishna Kant Pathak <krishnakantp002@gmail.com>
2026-02-01 20:07:58 +0530

    Input Validator Added

---
+++ src/middlewares/inputValidator.js
@@ -0,0 +1,20 @@
+const Joi = require('joi');
+
+const userSchema = Joi.object({
+    name : Joi.string().min(3).required(),
+    email : Joi.string().required()
+});
+
+const validateUser = (req,res,next) => {
+    const { error } = userSchema.validate(req.body);
+
+    if(error) 
+        return res.status(400).json({
+            status : 400,
+            message : error.details[0].message
+        });
+        next();
+    
+}
+
+module.exports=validateUser;
